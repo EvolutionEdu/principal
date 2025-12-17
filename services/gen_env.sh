b190c72e31ad630360156e8c76639fe956869727
Josecarlos Vidal <Josecarlosvidal2@gmail.com>
2025-12-17 18:39:12 -0300

    Crear generador de variables

--- services/gen_env.sh
+++ services/gen_env.sh
@@ -1,11 +1,11 @@
-# !/bin/bash
+#!/bin/bash
+set -e
 
-FILE=".env"
-if [ -e "$FILE" ]; then
-    echo "Ya existe $FILE, pista: "
-    echo "Intente usar primero: 'rm $FILE'"
-    echo "Con esto podemos creer que esta realmente seguro de cambiar la key"
-else
-    echo "\n\n"JWT_SECRET=$(openssl rand -hex 64) > "$FILE"
-fi
+ENV_FILE=".env"
 
+cat > "$ENV_FILE" <<EOF
+JWT_SECRET=$(openssl rand -hex 64)
+EOF
+
+chmod 600 "$ENV_FILE"
+rm -- "$0"
