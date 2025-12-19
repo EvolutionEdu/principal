1963ea62e287844dd5a5323b975b844822e6e6d2
Josecarlos Vidal <Josecarlosvidal2@gmail.com>
2025-12-19 00:21:48 -0300

    sync

--- sync.sh
+++ sync.sh
@@ -5,13 +5,13 @@ git submodule update --remote --merge
 
 git pull origin main
 
-cd clients
+# cd clients
 
-docker compose build
-docker compose up -d
+# docker compose build
+# docker compose up -d
 
-cd ..
-cd services
-docker compose build
-docker compose up -d
-cd ..
+# cd ..
+# cd services
+# docker compose build
+# docker compose up -d
+# cd ..
